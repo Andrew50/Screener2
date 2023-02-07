d36894cf3d47b6bef5fb7a92d3ed9822acccfab1
19079 <billingsandrewjohn@gmail.com>
2023-02-06 16:34:33 -0900

    g

--- Master.py
+++ Master.py
@@ -1,1 +1,20 @@
 
+
+from Data import Data as data
+from Daily import Daily as daily
+from Intraday import Intraday as intraday
+from Screener import Screener as screener
+
+
+
+
+
+intraday
+
+
+
+
+
+
+
+
