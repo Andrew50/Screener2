ac65b361546cfdfae254fe9d865287b08d316079
bliuak <cs.benliu@gmail.com>
2023-02-07 20:39:58 -0900

    wty rtwetATwtrygq2a4yqa42ry24aqt6y

--- discordManager.py
+++ discordManager.py
@@ -70,58 +70,59 @@ class discordManager:
         marketCap = round(screenbar['Market Capitalization'], 1)
         relativeVolAtTime = round(screenbar['Relative Volume at Time'], 1)
         todayGapValuePercent = dayChange*100;
-
-
-
         z = round(z, 3)
-        prevClose = setup_df.iloc[len(setup_df)][4]
+        prevClose = setup_df.iloc[len(setup_df)-2][4]
         pmPrice = round((prevClose + pmChange), 2)
+        marketCapText = round((marketCap / 1000000000), 2)
+        if(currentDay == "0"):
+            lengthDf = len(setup_df)
+            setup_df = setup_df[lengthDf - 80:]
+
 
         mc = mpf.make_marketcolors(up='g',down='r')
         s  = mpf.make_mpf_style(marketcolors=mc)
-
-
-
         ourpath = pathlib.Path("C:/Screener/tmp") / "test.png"
-        
-        mpf.plot(setup_df, type='candle', mav=(10, 20), volume=True, title=tick, vlines=dict(vlines=[date],linewidths=(1), alpha=0.25), hlines=dict(hlines=[pmPrice], linestyle="-."), style=s, savefig=ourpath)
-        discordManager.sendDiscordEmbed(tick + f" {prevClose} >> {pmPrice} ▲ {pmChange} ({todayGapValuePercent}%)", f"EP Setup, Z-Score: {z}")
-        discordManager.sendDiscordPost('tmp/test.png')
+        if(type == "Pop"):
+            openCandlePrice = float(setup_df.iloc[len(setup_df)-1][1])
+            changePrice = round(float(currPrice - openCandlePrice), 2)
+            mpf.plot(setup_df, type='candle', mav=(10, 20), volume=True, title=tick, style=s, savefig=ourpath)
+            discordManager.sendDiscordEmbedIntraday(tick + f" {openCandlePrice} >> Current: {currPrice} ▲ {changePrice} ({change}%)", f"Intraday % Gaining Setup, Volume: {volume}, RelVol: {relativeVolAtTime}x, MCap: ${marketCapText}B")
+            discordManager.sendDiscordIntradayPost('tmp/test.png')
 
-        z = round(z, 3)
-        ourpath = pathlib.Path("C:/Screener/tmp") / "test.png"
-        todayGapValuePercent = todayGapValue*100;
-        mpf.plot(data_daily, type='candle', mav=(10, 20), volume=True, title=tick, hlines=dict(hlines=[pmPrice], linestyle="-."), style=s, savefig=ourpath)
-        discordManager.sendDiscordEmbed(tick + f" {prevClose} >> {pmPrice} ▼ {pmChange} ({todayGapValuePercent}%)", f"NEP Setup, Z-Score: {z}")
-        discordManager.sendDiscordPost('tmp/test.png')
+        if(type == "Gainer"):
+            openCandlePrice = float(setup_df.iloc[len(setup_df)-1][1])
+            changePrice = round(float(currPrice - openCandlePrice), 2)
+            mpf.plot(setup_df, type='candle', mav=(10, 20), volume=True, title=tick, style=s, savefig=ourpath)
+            discordManager.sendDiscordEmbedGainers(tick + f" {openValue} >> {currPrice} ▲ {changeFromOpen} ({dayChange}%)", f"Top Gainer, Volume: {volume}, RelVol: {relativeVolAtTime}x, MCap: ${marketCapText}B")
+            discordManager.sendDiscordGainersPost('tmp/test.png')
+
+        if(currentDay == (len(df)-1)):
+            if(type == "MR"):
+                datetime = str(df.index[currentDay])
+                date = datetime.split()[0]
+                mpf.plot(df, type='candle', volume=True, title=tick, vlines=dict(vlines=[date],linewidths=(1), alpha=0.25), hlines=dict(hlines=[pmPrice], linestyle="-."), style=s, savefig=ourpath)
+                discordManager.sendDiscordEmbed(tick + f" {prevClose} >> {pmPrice} ▲ {pmChange} ({todayGapValuePercent}%)", f"MR Setup, Z-Score: {z}")
+                discordManager.sendDiscordyPost('tmp/test.png')
+            if(type == "EP"):
+                datetime = str(df.index[currentDay])
+                date = datetime.split()[0]
+                mpf.plot(df, type='candle', volume=True, title=tick, vlines=dict(vlines=[date],linewidths=(1), alpha=0.25), hlines=dict(hlines=[pmPrice], linestyle="-."), style=s, savefig=ourpath)
+                discordManager.sendDiscordEmbed(tick + f" {prevClose} >> {pmPrice} ▲ {pmChange} ({todayGapValuePercent}%)", f"MR Setup, Z-Score: {z}")
+                discordManager.sendDiscordyPost('tmp/test.png')
+        elif():
+            if(type == "MR"):
+                datetime = str(df.index[currentDay])
+                date = datetime.split()[0]
+                mpf.plot(df, type='candle', volume=True, title=tick, vlines=dict(vlines=[date],linewidths=(1), alpha=0.25), style=s, savefig=ourpath)
+                discordManager.sendDiscordEmbed(tick + f" {prevClose} >> {pmPrice} ▲ {pmChange} ({todayGapValuePercent}%)", f"MR Setup, Z-Score: {z}")
+                discordManager.sendDiscordyPost('tmp/test.png')
+            if(type == "EP"):
+                datetime = str(df.index[currentDay])
+                date = datetime.split()[0]
+                mpf.plot(df, type='candle', volume=True, title=tick, vlines=dict(vlines=[date],linewidths=(1), alpha=0.25), style=s, savefig=ourpath)
+                discordManager.sendDiscordEmbed(tick + f" {prevClose} >> {pmPrice} ▲ {pmChange} ({todayGapValuePercent}%)", f"MR Setup, Z-Score: {z}")
+                discordManager.sendDiscordyPost('tmp/test.png')
 
 
-        z = round(z, 3)
-        ourpath = pathlib.Path("C:/Screener/tmp") / "test.png"
-        todayGapValuePercent = todayGapValue*100;
-        mpf.plot(data_daily, type='candle', mav=(10, 20), volume=True, title=tick, hlines=dict(hlines=[pmPrice], linestyle="-."), style=s, savefig=ourpath)
-        discordManager.sendDiscordEmbed(tick + f" {prevClose} >> {pmPrice} ▲ {pmChange} ({todayGapValuePercent}%)", f"MR Setup, Z-Score: {z}")
-        discordManager.sendDiscordPost('tmp/test.png')
-
-
-
-        ourpath = pathlib.Path("C:/Screener/tmp") / "test3.png"
-                openCandlePrice = float(data_100.iloc[len(data_100)-1][1])
-                changePrice = round(float(currPrice - openCandlePrice), 2)
-            
-                marketCapText = round((marketCap / 1000000000), 2)
-            
-                mpf.plot(data_100, type='candle', volume=True, title=tick, style=s, savefig=ourpath)
-                dM.sendDiscordEmbedIntraday(tick + f" {openCandlePrice} >> Current: {currPrice} ▲ {changePrice} ({change}%)", f"Intraday % Gaining Setup, Volume: {volume}, RelVol: {relativeVolAtTime}x, MCap: ${marketCapText}B")
-                dM.sendDiscordIntradayPost('tmp/test3.png')
-
-
-                changeFromOpen = screener_data.iloc[i]['Change from Open']
-            openValue = screener_data.iloc[i]['Open']
-            
-            pmChange = screener_data.iloc[i]['Pre-market Change']
-            
-            marketCap = float(screener_data.iloc[i]['Market Capitalization'])
-            relativeVolAtTime = round(screener_data.iloc[i]['Relative Volume at Time'], 1)
 
 
