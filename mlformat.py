71a253d405b37c48ef1c84d9113649fb4344dcf6
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-05-24 17:47:30 -0800

    e

--- mlformat.py
+++ mlformat.py
@@ -1,10 +1,11 @@
 import os
 import numpy as np
 import pandas as pd
-from Data7 import Data
+from Data7 import Data as data
 TRAIN_SPLIT = 0.8
 FEAT_LENGTH = 50
-FEAT_COLS = ['Open', 'Low', 'High', 'Close']
+FEAT_COLS = ['open', 'low', 'high', 'close']
+#FEAT_COLS = ['Open', 'Low', 'High', 'Close']
 TICKERS = ['TSLA', 'AAPL', 'MSFT', 'NVDA', 'GOOG', 'AMD']
 
 def getTickers(): 
@@ -30,24 +31,27 @@ def get_lagged_returns(df: pd.DataFrame) -> pd.DataFrame:
     For each of the feature cols, find the returns and then form the lagged
     time-series as new columns
     '''
+  
     for col in FEAT_COLS:
-        print(df[col])
-        print(df[col].shift(1))
+        #print(df[col])
+        #print(df[col].shift(1))
         return_col = df[col]/df[col].shift(1)-1
-        print(return_col)
-        print("------------------------------")
+       # print(return_col)
+     #   print("------------------------------")
+        
         df = time_series(df, return_col, f'feat_{col}_ret')
+    #df.to_csv('C:/Screener/godddd.csv')
     return df
 
 
-def get_classification(df: pd.DataFrame) -> pd.DataFrame:
+def get_classification(df: pd.DataFrame,value) -> pd.DataFrame:
     '''
     Get the classifications for the LSTM network, which are as follows:
         0 = The 20 period SMA is below the low of the day
         1 = The 20 period SMA is between the low and high of the day
         2 = The 20 period SMA is above the high of the day
     '''
-    
+    '''
     df['ma'] = df['Close'].rolling(20).mean()
     
     conditions = [
@@ -60,7 +64,8 @@ def get_classification(df: pd.DataFrame) -> pd.DataFrame:
         condlist = conditions,
         choicelist = [0, 1, 2],
     )
-    
+    '''
+    df['classification'] = value
     return df
     
 
@@ -97,19 +102,44 @@ def get_nn_data():
     For all tickers, deduce the NN features and classifications, and then save
     the outputs as four numpy arrays (x_train, y_train, x_test, y_test)
     '''
-    
+    setup = 'EP'
+    setups = pd.read_feather('C:/Screener/setups/' + setup + '.feather')
+
+
+
+
+
+
     dfs = []
-    for ticker in TICKERS:
+    #for ticker in TICKERS:
+    for i in range(len(setups)):
+        ticker = setups.iat[i,0]
+        date = setups.iat[i,1]
+        value = setups.iat[i,2]
+
+        df = data.get(ticker)
+
+       
+        index = data.findex(df,date)
+        df2 = df[index-50:index]
+
+        o = df.iat[index,0]
+        add = pd.DataFrame({
+            'datetime':[date],
+            'open':[o],
+            'high':[o],
+            'low':[o],
+            'close':[o],
+            'volume':[0]}).set_index('datetime')
+        df2 = pd.concat([df2,add])
+        df = df2
 
 
-
-
-
-
-        df = pd.read_csv(f'data/{ticker}.csv')
-
+        #df = pd.read_csv(f'data/{ticker}.csv')
         df = get_lagged_returns(df)
-        df = get_classification(df)
+      #  print(df)
+        df = get_classification(df,value)
+     #   print(df)
         
         # We may end up with some divisions by 0 when calculating the returns
         # so to prevent any rows with this slipping in, we replace any infs
@@ -120,10 +150,17 @@ def get_nn_data():
             .dropna()
             [[col for col in df.columns if 'feat_' in col] + ['classification']]
         )
-        print(dfs)
-    nn_values = pd.concat(dfs).values
+        
+    nn_values = pd.concat(dfs)
+    nn_values.to_csv('C:/Screener/godddd.csv')
+    nn_values = nn_values.values
+
+    
+    #print(dfs)
     # Shuffle the values to ensure the NN does not learn an order
     np.random.shuffle(nn_values)
+
+
     
     # Split into training and test data
     split_idx = int(TRAIN_SPLIT*nn_values.shape[0])
