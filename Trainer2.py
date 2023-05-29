ac1588d3b9b641e68fbbbd299620561f5e72ed94
19079 <billingsandrewjohn@gmail.com>
2023-05-28 16:39:14 -0800

    d

--- Trainer2.py
+++ Trainer2.py
@@ -203,10 +203,14 @@ class Trainer:
 
     def loop(self):
 
-
-        self.height = 1200
-        self.width = 2500
-        self.cutoff = 75
+        elif os.path.exists("C:/Screener/ben.txt"):
+            self.height = 1200
+            self.width = 2500
+            self.cutoff = 75
+        else:
+            self.height = 1200
+            self.width = 2500
+            self.cutoff = 75
 
         self.size = 300
 
